0faf54918b6b3d4de0f0fa6d8bbc93d5f8198c88
TRIF3X <thewizardofcrypto@gmail.com>
2018-11-30 10:53:05 -0600

    mvp

--- config/routes.js
+++ config/routes.js
@@ -1,4 +1,8 @@
 const axios = require('axios');
+const jwt = require('jsonwebtoken')
+const secret = require('../_secrets/keys').jwtKey;
+const bcrypt = require('bcryptjs')
+const db = require('../database/dbConfig.js')
 
 const { authenticate } = require('./middlewares');
 
@@ -13,18 +17,59 @@ function test(req, res) {
   res.send('im working')
 }
 
+function generateToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username
+  }
+
+  const options = {
+    expiresIn: '1hr'
+  }
+
+  return jwt.sign(payload, secret, options)
+}
+
 function register(req, res) {
-  // implement user registration
+  const creds = req.body;
+
+  const hash = bcrypt.hashSync(creds.password, 14)
+  creds.password = hash;
+
+  db('users')
+    .insert(creds)
+    .then(ids => {
+      const id = ids[0]
+      res.status(201).json({ newUserId: id })
+    })
+    .catch(err => {
+      res.status(500).json({ message: 'error', err })
+    })
 }
 
 function login(req, res) {
-  // implement user login
+  const creds = req.body
+
+  db('users')
+    .where({ username: creds.username })
+    .first()
+    .then(user => {
+      if(user && bcrypt.compareSync(creds.password, user.password)) {
+        const token = generateToken(user);
+        res.status(200).json({ welcome: user.username, token })
+      } else {
+        res.status(401).json({ message: 'Error logging in' })
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ message: 'server error', err })
+    })
 }
 
 function getJokes(req, res) {
   axios
     .get(
-      'https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_ten'
+      'https://safe-falls-22549.herokuapp.com/random_ten'
     )
     .then(response => {
       res.status(200).json(response.data);
